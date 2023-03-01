b9747c4ed9b18475e75b4bf13dcb5a5acb0875ca
Logon Aluno <logonrmlocal@fiap.com.br>
2023-03-01 20:51:32 -0300

    exercicios

--- William_Banco.sql
+++ William_Banco.sql
@@ -223,7 +223,36 @@ update nome_tabela set nome_coluna = novo_valor
 update nome_tabela set nome_coluna = novo_valor
 where condição
 
-pegar o resto da aula no site
+create table produto_tb 
+(cod_prod number(4) constraint prod_cod_pk primary key, 
+unidade varchar2(3),descricao varchar2(20),val_unit number(10,2))
 
+insert into produto_tb values ('25','KG','Queijo','0.97');
+insert into produto_tb values ('31','BAR','Chocolate','0.87');
+insert into produto_tb values ('78','L','Vinho','2.00');
+insert into produto_tb values ('22','M','Linho','0.11');
+insert into produto_tb values ('30','SAC','Acucar','0.30');
+insert into produto_tb values ('53','M','Linha','1.80');
+insert into produto_tb values ('13','G','Ouro','6.18');
+insert into produto_tb values ('45','M','Madeira','0.25');
+insert into produto_tb values ('87','M','Cano','1.97');
+insert into produto_tb values ('77','M','Papel','1.05');
+commit;
+
+ex:
+
+1- Atualizar em 15% o preço dos produtos de codigo maior que 30.
+update produto_tb set val_unit = val_unit * 1.15
+where cod_prod > 30
+
+2- atualizar o nome do produto queijo para queijo minas.
+update produto_tb set descricao = 'Queijo Minas'
+where descricao = 'Queijo'
+
+3- para os produtos Açucar, madeira e linha zerar o preço.
+update produto_tb set val_unit = 0
+where cod_prod = 30 or cod_prod = 45 or cod_prod = 53
+
+select * from produto_tb order by 1
 
 
